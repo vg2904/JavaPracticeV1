c42e05ca55dc90ae1f673f127f283be7ed0979b1
vinay <vinay.test01@gmail.com>
2023-08-07 01:20:21 +0530

    commit 3

--- src/practice/PracticeSheetMain.java
+++ src/practice/PracticeSheetMain.java
@@ -1,12 +1,20 @@
 package practice;
 
 public class PracticeSheetMain {
-
     public static void main(String[] args) {
 
-        PracticeSheet emp = new PracticeSheet();
-        emp.empt(101, "simla", "eng");
-        emp.display();
-
+//        System.out.println("program startsss");
+//        String a = "456";
+//        int x = Integer.parseInt(a);
+//        int  y = 30;
+//
+//        try {
+//            System.out.println(y/0);
+//        }
+//        catch (){
+//
+//        }
+//        System.out.println(y/0);
+//        System.out.println("program endsssss");
     }
 }
